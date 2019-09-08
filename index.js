7d8626a15300b7f884e0d8fdfcb528768623b848
Miki Ishibashi <52851340+im1990@users.noreply.github.com>
2019-09-08 10:42:55 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const fizz = document.getElementById('fizz');
+const buzz = document.getElementById('buzz');
+const submit = document.getElementById('submit');
+const output = document.querySelector('.output');
+
+submit.addEventListener('click', function() {
+
+    if(!isNaN(fizz.value) && !isNaN(buzz.value)){
+        // output.innerHTML = `<br>Fizz ${fizz.value * 1}`;
+        // output.innerHTML = `<br>Buzz ${buzz.value * 1}`;
+        output.innerHTML = "";
+
+        for(let i = 2; i <= 100; i++){
+            if(i % fizz.value === 0 && i % buzz.value === 0){
+                output.innerHTML += `<br>FizzBuzz ${i}`;
+            } else if (i % fizz.value === 0){
+                output.innerHTML += `<br>Fizz ${i}`;
+            } else if (i % buzz.value === 0){
+                output.innerHTML += `<br>Buzz ${i}`;
+            }
+        }
+        
+    } else {
+        output.innerHTML = "整数値を入力してください";
+    }
+
+});
+
+
+
